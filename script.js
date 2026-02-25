abbac96d0c93b17a69e32c50ff4c56245a448914
Your name <Your email>
2026-02-25 19:59:44 +0100

    ok3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('https://weatherapp-part4-backend-zeta.vercel.app/year')
+fetch('https://whatyearisit-backend-phi-inky.vercel.app/year')
  .then(response => response.json())
  .then(data => {
    document.querySelector('#year').textContent = data.year;
